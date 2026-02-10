49687d809116d18af31db90c53f2720827e8323b
KrishnaDhiman01 <krishnadhiman5560@gmail.com>
2026-02-10 22:04:57 +0530

    Fixed media configuration and added default profile image

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,5 +9,8 @@ urlpatterns = [
     # Authentication
     path('signup/', views.signup_view, name='signup'),
     path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='accounts/logout.html'), name='logout'),
+    path('profile/<str:username>/', views.profile_view, name='profile'),
+    path('edit-profile/', views.edit_profile_view, name='edit_profile'),
+    path('logout/', views.logout_view, name='logout'),
 ]
+
--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from django.contrib.auth import login
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
+from django.shortcuts import redirect
 
 def signup_view(request):
     if request.method == 'POST':
@@ -18,3 +23,30 @@ def signup_view(request):
 
 def feed_view(request):
     return render(request, 'accounts/base.html', {'title': 'Feed'})
+
+def profile_view(request, username):
+    # Get the user by their username (e.g., 'krishna')
+    user_obj = get_object_or_404(User, username=username)
+    
+    # Send their data to the template
+    return render(request, 'accounts/profile.html', {'user_obj': user_obj})
+
+@login_required
+def edit_profile_view(request):
+    if request.method == 'POST':
+        # Get data from the form
+        user = request.user
+        user.profile.bio = request.POST.get('bio')
+        
+        # Check if an image was uploaded
+        if 'profile_img' in request.FILES:
+            user.profile.profile_img = request.FILES['profile_img']
+        
+        user.profile.save()
+        return redirect('profile', username=user.username)
+
+    return render(request, 'accounts/edit_profile.html')
+
+def logout_view(request):
+    logout(request)
+    return render(request, 'accounts/logout.html')
